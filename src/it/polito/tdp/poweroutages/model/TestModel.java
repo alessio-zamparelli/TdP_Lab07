b6ad796c5166e6a2cd73a33a55534c11d8443b90
alessio-zamparelli <37191190+alessio-zamparelli@users.noreply.github.com>
2019-05-26 22:20:51 +0200

    a

--- src/it/polito/tdp/poweroutages/model/TestModel.java
+++ src/it/polito/tdp/poweroutages/model/TestModel.java
@@ -12,14 +12,19 @@ public class TestModel {
 		System.out.println("Nerc List size: " + nercList.size());
 		
 		Nerc selectedNerc = nercList.get(3);
-		List<PowerOutageEvent> worstCase = model.getWorstCase(3, 250, selectedNerc);
+//		List<PowerOutageEvent> worstCase = model.getWorstCase(3, 250, selectedNerc);
+		long start = System.nanoTime();
+		List<PowerOutageEvent> res = model.getWorstCase(3, 250, nercList.stream().filter(a -> a.getId() == 3).findAny().orElse(null));
+		System.out.println(res);
+		long end = System.nanoTime();
 		
-		System.out.println("Tot people affected: " + model.sumAffectedPeople(worstCase));
-		System.out.println("Tot hours of outage: " + model.sumOutageHours(worstCase));
-		
-		for (PowerOutageEvent event : worstCase) {
-			System.out.println(String.format("%d %s %s %d %d", event.getYear(), event.getOutageStart(),
-					event.getOutageEnd(), event.getOutageDuration(), event.getAffectedPeople()));
-		}
+		System.out.format("\n\nTempo impiegato %.2f", (end-start)/1e6);
+//		System.out.println("Tot people affected: " + model.sumAffectedPeople(worstCase));
+//		System.out.println("Tot hours of outage: " + model.sumOutageHours(worstCase));
+//		
+//		for (PowerOutageEvent event : worstCase) {
+//			System.out.println(String.format("%d %s %s %d %d", event.getYear(), event.getOutageStart(),
+//					event.getOutageEnd(), event.getOutageDuration(), event.getAffectedPeople()));
+//		}
 	}
 }
